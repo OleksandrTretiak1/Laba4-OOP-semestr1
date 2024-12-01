463f846179a089fed17ed5f714de05e70601bafb
OleksandrTretiak1 <ssanka467@gmail.com>
2024-12-01 19:36:43 +0200

    Cahnged a little bit MyFrac

--- Лабораторна робота 4 ООП (1 семестр)/MyFrac.cs
+++ Лабораторна робота 4 ООП (1 семестр)/MyFrac.cs
@@ -32,32 +32,6 @@ namespace Laba_4
             return BigInteger.Abs(a);
         }
 
-        //public MyFrac(int _nom,int _denom)
-        //{
-        //    if (_nom != 0)
-        //    {
-        //        int a = _nom;
-        //        int b = _denom;
-        //        while (b != 0)
-        //        {
-        //            if (a > b)
-        //            {
-        //                a = a - b;
-        //            }
-        //            else
-        //            {
-        //                b = b - a;
-        //            }
-        //        }
-
-        //        nom = (_denom > 0) ? _nom / a : -(_nom / a);
-        //        denom = (_denom > 0) ? _denom / a : Math.Abs(_denom) / a;
-        //    }
-        //    else
-        //    {
-        //        nom = 0; denom = _denom;
-        //    }
-        //}
         public MyFrac Add(MyFrac that)
         {
             BigInteger new_nom = this.nom * that.denom + that.nom * this.denom;
