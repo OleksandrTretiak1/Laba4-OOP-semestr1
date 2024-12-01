b0b51f80d48bc1292ffd216fd21fdcae4ab1e757
OleksandrTretiak1 <ssanka467@gmail.com>
2024-12-01 18:58:35 +0200

    Add all files what i need

--- Лабораторна робота 4 ООП (1 семестр)/Test_Complex.cs
+++ Лабораторна робота 4 ООП (1 семестр)/Test_Complex.cs
@@ -1,6 +1,6 @@
 namespace Лабораторна_робота_4_ООП__1_семестр_
 {
-    public class UnitTest1
+    public class Test_Complex
     {
         [Fact]
         public void Test1()
---
+++ Лабораторна робота 4 ООП (1 семестр)/Test_My_Frac.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лабораторна_робота_4_ООП__1_семестр_
+{
+    internal class Test_My_Frac
+    {
+    }
+}
